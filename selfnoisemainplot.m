a54ee5c5568c68618b8bb1fc6e180ed150348c30
aringler-usgs <aringler@usgs.gov>
2013-09-24 07:59:15 -0600

    Made a self-noise version of the code using Sleeman's self-noise

---
+++ selfnoisemainplot.m
@@ -0,0 +1,427 @@
+function selfnoisemainplot
+% SIMPLE_GUI2 Select a data set from the pop-up menu, then
+% click one of the plot-type push buttons. Clicking the button
+% plots the selected data in the axes.
+ 
+    digtype={'Q330HR','Q330','Q330HRx20','Q330x20'};
+    sentype={'STS-1','STS-1t5','STS-2SGgen1','STS-2SGgen2', ...
+        'STS-2SGgen3', 'STS-2HGgen1', 'STS-2HGgen2', 'STS-2HGgen3', ...
+        'STS-2.5', 'KS-54000','KS-36000','CMG-3T','TR-240','TR-120', 'TR-CMPT', ...
+        'Titan','147-01/3','Epi-EST','PA-23'};
+    
+    nlnm=load('auxfiles/NLNM.txt');
+    nhnm=load('auxfiles/NHNM.txt');
+
+
+    %  Create and then hide the GUI as it is being constructed.
+    f = figure('Visible','off','Position',[1060,500,850,850]);
+ 
+    %  Construct the components.
+    hchan1 = uicontrol('Style','pushbutton','String','Channel 1',...
+          'Position',[750,800,70,25],...
+          'Callback',{@sen1button_Callback});
+    hsen1 = uicontrol('Style','text','String','Sensor 1 Type',...
+          'Position',[750,770,100,15]);
+    hpopupsen1 = uicontrol('Style','popupmenu',...
+          'String',sentype,...
+          'Position',[750,740,100,25],...
+          'Callback',{@popup_menu_Callback_sen_1});
+    hdig1 = uicontrol('Style','text','String','Digitizer 1 Type',...
+          'Position',[750,710,100,15]);
+    hpopupdig1 = uicontrol('Style','popupmenu',...
+          'String',digtype,...
+          'Position',[750,680,100,25],...
+          'Callback',{@popup_menu_Callback_dig_1});
+   
+    hchan2 = uicontrol('Style','pushbutton','String','Channel 2',...
+          'Position',[750,640,70,25],...
+          'Callback',{@sen2button_Callback});
+    hsen2 = uicontrol('Style','text','String','Sensor 2 Type',...
+          'Position',[750,610,100,15]);
+    hpopupsen2 = uicontrol('Style','popupmenu',...
+          'String',sentype,...
+          'Position',[750,580,100,25],...
+          'Callback',{@popup_menu_Callback_sen_2});
+    hdig2 = uicontrol('Style','text','String','Digitizer 2 Type',...
+          'Position',[750,550,100,15]);
+    hpopupdig2 = uicontrol('Style','popupmenu',...
+          'String',digtype,...
+          'Position',[750,520,100,25],...
+          'Callback',{@popup_menu_Callback_dig_2});
+      
+    hchan3 = uicontrol('Style','pushbutton',...
+          'String','Channel 3',...
+          'Position',[750,480,70,25],...
+          'Callback',{@sen3button_Callback}); 
+    hsen3 = uicontrol('Style','text','String','Sensor 3 Type',...
+          'Position',[750,450,100,15]);
+    hpopupsen3 = uicontrol('Style','popupmenu',...
+          'String',sentype,...
+          'Position',[725,420,100,25],...
+          'Callback',{@popup_menu_Callback_sen_3});
+    hpopupdig3 = uicontrol('Style','popupmenu',...
+          'String',digtype,...
+          'Position',[700,360,100,25],...
+          'Callback',{@popup_menu_Callback_dig_3});
+    hdig3 = uicontrol('Style','text','String','Digitizer 3 Type',...
+          'Position',[700,390,100,15]);
+    plotme = uicontrol('Style','pushbutton',...
+          'String','Plot Me',...
+          'Position',[700,310,100,25],...
+          'Callback',{@plot_Callback}); 
+    clearme = uicontrol('Style','pushbutton',...
+          'String','Clear Data',...
+          'Position',[700,280,100,25],...
+          'Callback',{@clearme_Callback});
+    saveme = uicontrol('Style','pushbutton',...
+          'String','Save Plot',...
+          'Position',[700,240,100,25],...
+          'Callback',{@saveme_Callback});
+    starttime = uicontrol('Style','edit',...
+          'String','00:00',...
+          'Position',[700,210,100,15],...
+          'Callback',{@starttime_Callback});
+    hstime = uicontrol('Style','text','String','Start Time',...
+          'Position',[750,190,100,15]); 
+    endtime = uicontrol('Style','edit',...
+          'String','24:00',...
+          'Position',[700,150,100,15],...
+          'Callback',{@endtime_Callback});
+      hetime = uicontrol('Style','text','String','End Time',...
+          'Position',[750,130,100,15]); 
+      
+    lowpasscon = uicontrol('Style','pushbutton',...
+          'String','Low Pass',...
+          'Position',[700,90,100,25],...
+          'Callback',{@lowpass_Callback});
+      
+    ha = axes('Units','Pixels','Position',[100,300,480,450]); 
+    align([hchan1,hsen1,hchan2,hsen2,hchan3,hsen3,hpopupsen1, ...
+        hpopupsen2, hpopupsen3, hpopupdig1, hpopupdig2, hpopupdig3, ...
+        hdig1, hdig2, hdig3, plotme, clearme, saveme, ...
+        starttime, endtime, hstime, hetime, lowpasscon],'Center','None');
+   
+    datafile1=[];
+    datafile2=[];
+    datafile3=[];
+    dig1='Q330HR';
+    dig2='Q330HR';
+    dig3='Q330HR';
+    sen1='STS-1';
+    sen2='STS-1';
+    sen3='STS-1';
+    stahand1='';
+    stahand2='';
+    stahand3='';
+    etimestr='';
+    stimestr='';
+    lowpass=0;
+   
+   % Initialize the GUI.
+   % Change units to normalized so components resize 
+   % automatically.
+   set([f,ha,hchan1,hsen1,hchan2,hsen2,hchan3,hsen3,hpopupsen1, ...
+       hpopupsen2, hpopupsen3, hpopupdig1, hpopupdig2, hpopupdig3, ...
+       hdig1, hdig2, hdig3, plotme, clearme, saveme, ...
+       starttime, endtime, hstime, hetime, lowpasscon],...
+   'Units','normalized');
+   %Create a plot in the axes.
+   subplot(10,6,[ 1 2 3 4 5 7 8 9 10 11 13 14 15 16 17 19 20 21 22 23 ] )
+   p1=semilogx(nlnm(:,1),nlnm(:,4),'color','k','LineWidth',3);
+   hold on
+   semilogx(nhnm(:,1),nhnm(:,2),'color','k','LineWidth',3);
+   set(gca,'FontSize',14);
+   xlim([.01 1000]);
+   ylim([-200 -80]);
+   legend(p1,'NLNM','FontSize',14,'Location','NorthWest');
+   hold off
+   title('Noise Estimate','FontSize',14);
+   xlabel('Period (s)','FontSize',14);
+   ylabel('Power (dB)','FontSize',14);
+   subplot(10,6,[ 31 32 33 34 35])
+   plot(-1,-1)
+   xlim([0 1000]);
+   xlabel('Time (HH:MM)','FontSize',14);
+   title('Channel 1 Time Series','FontSize',14);
+   set(gca,'fontsize',14);
+   ylabel('Volts');
+   subplot(10,6,[ 43 44 45 46 47])
+   plot(-1,-1)
+   xlim([0 1000]);
+   xlabel('Time (HH:MM)','FontSize',14);
+   title('Channel 2 Time Series','FontSize',14);
+   set(gca,'fontsize',14);
+   ylabel('Volts');
+   subplot(10,6,[ 55 56 57 58 59])
+   plot(-1,-1)
+   xlim([0 1000]);
+   set(gca,'fontsize',14);
+   xlabel('Time (HH:MM)','FontSize',14);
+   ylabel('Volts');
+   title('Channel 3 Time Series','FontSize',14);
+   
+   
+   
+   
+   % Assign the GUI a name to appear in the window title.
+    set(f,'Name','Self-Noise Estimate')
+   % Move the GUI to the center of the screen.
+    movegui(f,'center')
+   % Make the GUI visible.
+    set(gcf, 'color', [.7 .7 .7]);
+    set(gcf,'MenuBar','none','ToolBar','none');
+    set(f,'Visible','on');
+  
+   
+    function popup_menu_Callback_sen_1(source,~) 
+        str = get(source, 'String');
+        val = get(source,'Value');
+        sen1=str{val};
+    end
+
+    function popup_menu_Callback_sen_2(source,~) 
+        str = get(source, 'String');
+        val = get(source,'Value');
+        sen2=str{val};
+    end
+
+    function popup_menu_Callback_sen_3(source,~) 
+        str = get(source, 'String');
+        val = get(source,'Value');
+        sen3=str{val};
+    end
+
+    function popup_menu_Callback_dig_1(source,~) 
+        str = get(source, 'String');
+        val = get(source,'Value');
+        dig1=str{val};
+    end
+
+    function popup_menu_Callback_dig_2(source,~) 
+        str = get(source, 'String');
+        val = get(source,'Value');
+        dig2=str{val};
+    end
+
+    function popup_menu_Callback_dig_3(source,~) 
+        str = get(source, 'String');
+        val = get(source,'Value');
+        dig3=str{val};
+    end
+  
+    % Push button callbacks. Each callback plots current_data in
+    % the specified plot type.
+ 
+    function sen1button_Callback(~,~) 
+        if(~isempty(datafile3))
+            direstart = datafile3;
+            if(strcmp(computer,'PCWIN64') || strcmp(computer,'PCWIN'))
+                extend = strfind(direstart,'\');
+            else
+                extend = strfind(direstart,'/');
+            end
+            direstart = direstart(1:extend(length(extend)));
+            [FileName,PathName] = uigetfile('*.seed', ...
+                'Select the Seed File', direstart);
+        else
+            [FileName,PathName] = uigetfile('*.seed', ...
+                'Select the Seed File');
+        end
+        datafile1 = fullfile(PathName,FileName);
+    end
+ 
+    function sen2button_Callback(~,~)
+        if(~isempty(datafile1))
+            direstart = datafile1;
+            if(strcmp(computer,'PCWIN64') || strcmp(computer,'PCWIN'))
+                extend = strfind(direstart,'\');
+            else
+                extend = strfind(direstart,'/');
+            end
+            direstart = direstart(1:extend(length(extend)));
+            [FileName,PathName] = uigetfile('*.seed', ...
+                'Select the Seed File', direstart);
+        else
+            [FileName,PathName] = uigetfile('*.seed', ...
+                'Select the Seed File');
+        end
+        datafile2 = fullfile(PathName,FileName);
+    end
+ 
+    function sen3button_Callback(~,~)
+        if(~isempty(datafile2))
+            direstart = datafile2;
+            if(strcmp(computer,'PCWIN64') || strcmp(computer,'PCWIN'))
+                extend = strfind(direstart,'\');
+            else
+                extend = strfind(direstart,'/');
+            end
+            direstart = direstart(1:extend(length(extend)));
+            [FileName,PathName] = uigetfile('*.seed', ...
+                'Select the Seed File', direstart);
+        else
+            [FileName,PathName] = uigetfile('*.seed', ...
+                'Select the Seed File');
+        end
+        datafile3 = fullfile(PathName,FileName);
+    end 
+
+    function plot_Callback(~,~)
+        phand1=[];
+        phand2=[];
+        phand3=[];
+        stahand1=[];
+        stahand2=[];
+        stahand3=[];
+        if(~isempty(datafile1) && ~isempty(datafile2) && ~isempty(datafile3))
+            [pspec1, stahand1, tseries1,ttime1, mabsvolt1, ...
+                pspec2, stahand2, tseries2,ttime2, mabsvolt2, ...
+                pspec3, stahand3, tseries3,ttime3, mabsvolt3, per, ...
+                np1, np2, np3, ...
+                ] = sleeman(datafile1, sen1,dig1, datafile2, sen2, ...
+                dig2, datafile3, sen3, dig3, stimestr,etimestr, lowpass);
+           
+            thand1=stahand1(length(stahand1)-8:length(stahand1));
+            stahand1=stahand1(1:length(stahand1)-8);
+            subplot(10,6,[ 1 2 3 4 5 7 8 9 10 11 13 14 15 16 17 19 20 21 22 23 ] )
+            phand1=semilogx(per,pspec1,'r:'); 
+            
+            hold on
+            phand1n=semilogx(per,np1,'r');
+            phand2=semilogx(per,pspec2,'b:');  
+            phand2n=semilogx(per,np2,'b');
+            thand3=stahand3(length(stahand3)-8:length(stahand3));
+            stahand3=stahand3(1:length(stahand3)-8);
+    
+            phand3=semilogx(per,pspec3,'g:');  
+       
+            phand3n=semilogx(per,np3,'g');
+   
+      
+            thand2=stahand2(length(stahand2)-8:length(stahand2));
+            stahand2=stahand2(1:length(stahand2)-8);
+            subplot(10,6,[ 31 32 33 34 35])
+            plot(ttime1,tseries1,'color','r');
+            datetick('x',15)
+            xlabel('Time (HH:MM)','FontSize',14);
+            ylabel('Volts');
+            title(['Channel 1 Time Series ' thand1 ' ' ...
+                num2str(roundn(1000*mabsvolt1,-2)) ' mV'],'FontSize',14);
+            set(gca,'fontsize',14);
+            subplot(10,6,[ 43 44 45 46 47])
+            plot(ttime2,tseries2,'color','b');
+            datetick('x',15)
+            xlabel('Time (HH:MM)','FontSize',14);
+            ylabel('Volts');
+            title(['Channel 2 Time Series ' thand2 ' ' ...
+                num2str(roundn(1000*mabsvolt2,-2)) ' mV'],'FontSize',14);
+            set(gca,'fontsize',14);    
+      
+            
+            subplot(10,6,[ 55 56 57 58 59])
+            plot(ttime3,tseries3,'color','g');
+            datetick('x',15)
+            xlabel('Time (HH:MM)','FontSize',14);
+            ylabel('Volts');
+            title(['Channel 3 Time Series ' thand3 ' ' ...
+                num2str(roundn(1000*mabsvolt3,-2)) ' mV'],'FontSize',14);
+            set(gca,'fontsize',14);  
+        end  
+        subplot(10,6,[ 1 2 3 4 5 7 8 9 10 11 13 14 15 16 17 19 ...
+            20 21 22 23 ] );
+        hold on
+        p1=semilogx(nlnm(:,1),nlnm(:,4),'color','k','LineWidth',3);
+        semilogx(nhnm(:,1),nhnm(:,2),'color','k','LineWidth',3);
+        set(gca,'FontSize',14);
+        title('Self-Noise Estimate','FontSize',14);
+        xlabel('Period (s)','FontSize',14);
+        ylabel('Power (dB)','FontSize',14);
+        xlim([.01 1000]);
+
+        ylim([-200 -80]);
+        if(length([phand1 phand2 phand3]) == 3);
+            legend([phand1 phand1n phand2 phand2n phand3 phand3n p1], ...
+                stahand1, [stahand1 ' Noise'], stahand2, [stahand2 ' Noise'] , ...
+                stahand3,[stahand3 ' Noise'],'NLNM','Location','NorthWest');
+        else
+            legend(p1,'NLNM','Location','NorthWest');
+        end
+            
+        
+        hold off
+        subplot(10,6,[ 43 44 45 46 47])
+        
+        
+    end
+
+    function clearme_Callback(~,~)
+        subplot(10,6,[ 1 2 3 4 5 7 8 9 10 11 13 14 15 16 17 19 20 21 ...
+            22 23 ] )
+        p1=semilogx(nlnm(:,1),nlnm(:,4),'color','k','LineWidth',3);
+        hold on
+        semilogx(nhnm(:,1),nhnm(:,2),'color','k','LineWidth',3);
+        set(gca,'FontSize',14);
+        xlim([.01 1000]);
+        ylim([-200 -80]);
+        legend(p1,'NLNM','FontSize',14,'Location','NorthWest');
+        hold off
+        title('Self-Noise Estimate','FontSize',14);
+        xlabel('Period (s)','FontSize',14);
+        ylabel('Power (dB)','FontSize',14);
+        subplot(10,6,[ 31 32 33 34 35])
+        plot(-1,-1)
+        xlim([0 1000]);
+        xlabel('Time (HH:MM)','FontSize',14);
+        title('Channel 1 Time Series','FontSize',14);
+        set(gca,'fontsize',14);
+        ylabel('Volts');
+        subplot(10,6,[ 43 44 45 46 47])
+        plot(-1,-1)
+        xlim([0 1000]);
+        xlabel('Time (HH:MM)','FontSize',14);
+        title('Channel 2 Time Series','FontSize',14);
+        set(gca,'fontsize',14);
+        ylabel('Volts');
+        subplot(10,6,[ 55 56 57 58 59])
+        plot(-1,-1)
+        xlim([0 1000]);
+        set(gca,'fontsize',14);
+        xlabel('Time (HH:MM)','FontSize',14);
+        ylabel('Volts');
+        title('Channel 3 Time Series','FontSize',14);
+        datafile1=[];
+        datafile2=[];
+        datafile3=[];
+        stahand2='';
+        stahand3='';
+        
+        
+    end
+    function saveme_Callback(~,~)
+        orient LandScape
+        printstr=regexprep([stahand1 stahand2 stahand3],' ','');
+        if(~isempty(printstr))
+            print('-dpdf',[printstr '.pdf']);
+        end
+    end
+    
+    function starttime_Callback(source,~) 
+        str = get(source, 'String');
+        stimestr=str;
+    end
+        
+    function endtime_Callback(source,~) 
+        str = get(source, 'String');
+        etimestr=str;
+    end    
+    
+    function lowpass_Callback(~,~) 
+        if(lowpass == 0)
+            lowpass=1;
+        else
+            lowpass=0;
+        end
+    end    
+
+ 
+end 
---
+++ sleeman.m
@@ -0,0 +1,194 @@
+function [pspec1, stahand1, tseries1, ttime1, mabsvolt1, ...
+    pspec2, stahand2, tseries2, ttime2, mabsvolt2, ...
+    pspec3, stahand3, tseries3, ttime3, mabsvolt3, per, np1, np2, np3 ...
+    ] = sleeman(noisedata1, sensor1,digitizer1, ...
+    noisedata2, sensor2, digitizer2, ...
+    noisedata3,sensor3, digitizer3, stimestr,etimestr,lowpass)
+    %Compute spectra and correct for response
+    %Read in data
+    try
+        [noisedata1,~]=rdmseed(noisedata1);
+    catch ME
+        display('Error reading in data 1 \n');
+    end
+    try
+        [noisedata2,~]=rdmseed(noisedata2);
+    catch ME
+        display('Error reading in data 2 \n');
+    end
+    try
+        [noisedata3,~]=rdmseed(noisedata3);
+    catch ME
+        display('Error reading in data 3 \n');
+    end
+
+    net1=deblank(noisedata1(1,1).NetworkCode);
+    net2=deblank(noisedata2(1,1).NetworkCode);
+    net3=deblank(noisedata3(1,1).NetworkCode);
+    sta1=deblank(noisedata1(1,1).StationIdentifierCode);
+    sta2=deblank(noisedata2(1,1).StationIdentifierCode);
+    sta3=deblank(noisedata3(1,1).StationIdentifierCode);
+    loc1=deblank(noisedata1(1,1).LocationIdentifier);
+    loc2=deblank(noisedata2(1,1).LocationIdentifier);
+    loc3=deblank(noisedata3(1,1).LocationIdentifier);
+    chan1=deblank(noisedata1(1,1).ChannelIdentifier);
+    chan2=deblank(noisedata2(1,1).ChannelIdentifier);
+    chan3=deblank(noisedata3(1,1).ChannelIdentifier);
+    if(noisedata1(1,1).SampleRate ~= noisedata2(1,1).SampleRate || ...
+        noisedata2(1,1).SampleRate ~= noisedata3(1,1).SampleRate)
+        display('Sample Rates are not equal \n');
+    end
+    sps=noisedata1(1,1).SampleRate;
+    year1=noisedata1(1,1).RecordStartTimeISO;
+    year2=noisedata2(1,1).RecordStartTimeISO;
+    year3=noisedata3(1,1).RecordStartTimeISO;
+    year1=year1(1:8);
+    day1=year1(6:8);
+    year1=year1(1:4);
+    year2=year2(1:8);
+    day2=year2(6:8);
+    year2=year2(1:4);
+    year3=year3(1:8);
+    day3=year3(6:8);
+    year3=year3(1:4);
+    noisetime1=cat(1,noisedata1.t);
+    noisedata1=cat(1,noisedata1.d);
+    noisetime2=cat(1,noisedata2.t);
+    noisedata2=cat(1,noisedata2.d);
+    noisetime3=cat(1,noisedata3.t);
+    noisedata3=cat(1,noisedata3.d);
+    if(~isempty(stimestr))
+        stimeind=strfind(stimestr,':');
+        stimehour = str2double(stimestr(1:stimeind-1));
+        stimemin = str2double(stimestr(stimeind+1:length(stimestr)));
+        stime = datenum(str2double(year1),01,str2double(day1), ...
+            stimehour,stimemin,0);
+        noisedata1=noisedata1(find(noisetime1>= stime));
+        noisetime1=noisetime1(find(noisetime1>= stime));
+        noisedata2=noisedata2(find(noisetime2>= stime));
+        noisetime2=noisetime2(find(noisetime2>= stime));
+        noisedata3=noisedata3(find(noisetime3>= stime));
+        noisetime3=noisetime3(find(noisetime3>= stime));
+    end
+    if(~isempty(etimestr))
+        etimeind=strfind(etimestr,':');
+        etimehour = str2double(etimestr(1:etimeind-1));
+        etimemin = str2double(etimestr(etimeind+1:length(etimestr)));
+        etime = datenum(str2double(year1),01,str2double(day1), ...
+            etimehour,etimemin,0);
+        noisedata=noisedata(find(noisetime<= etime));
+        noisetime=noisetime(find(noisetime<= etime));
+    end
+    
+    overlap = floor(.66*length(noisedata1)/10);
+    win = floor(length(noisedata1)/10);
+
+    %Compute the spectra
+	[pspec1,fre]=cpsd(noisedata1,noisedata1,win,overlap, win , sps);
+    pspec1 = pspec1/sps;
+    [pspec2,fre]=cpsd(noisedata2,noisedata2,win,overlap, win , sps);
+    pspec2 = pspec2/sps;
+    [pspec3,fre]=cpsd(noisedata3,noisedata3,win,overlap, win , sps);
+    pspec3 = pspec3/sps;
+    %Lets compute the cross-power
+    [pspec21, fre]=cpsd(noisedata2,noisedata1,win,overlap, win , sps);
+    pspec21 = pspec21/sps;
+    [pspec13, fre]=cpsd(noisedata1,noisedata3,win,overlap, win , sps);
+    pspec13 = pspec13/sps;
+    [pspec23, fre]=cpsd(noisedata2,noisedata3,win,overlap, win , sps);
+    pspec23 = pspec23/sps;
+    
+    
+    
+    
+    %Get the response and correct
+    [zer,pol,gai1]=getrespmodel(sensor1);
+    resp1=1;
+    for ind=1:length(pol)
+        resp1=resp1./(2*pi*1i*fre-pol(ind));
+    end
+    for ind=1:length(zer)
+        resp1=resp1.*(2*pi*1i*fre-zer(ind));
+    end
+    [zer,pol,gai2]=getrespmodel(sensor2);
+    resp2=1;
+    for ind=1:length(pol)
+        resp2=resp2./(2*pi*1i*fre-pol(ind));
+    end
+    for ind=1:length(zer)
+        resp2=resp2.*(2*pi*1i*fre-zer(ind));
+    end
+    [zer,pol,gai3]=getrespmodel(sensor3);
+    resp3=1;
+    for ind=1:length(pol)
+        resp3=resp3./(2*pi*1i*fre-pol(ind));
+    end
+    for ind=1:length(zer)
+        resp3=resp3.*(2*pi*1i*fre-zer(ind));
+    end
+    
+
+    digsen1=getdigitmodel(digitizer1);
+
+    resp1=resp1/abs(resp1(find(fre<.1  ,1,'last')));
+    resp2=resp2/abs(resp2(find(fre<.1  ,1,'last')));
+    resp3=resp3/abs(resp3(find(fre<.1  ,1,'last')));
+    if(~strcmp(sensor1,'Epi-EST') && ~strcmp(sensor1,'Titan') && ...
+            ~strcmp(sensor1,'CMG-5T') && ~strcmp(sensor1,'147-01/3') && ...
+            ~strcmp(sensor1,'PA-23'))
+        resp1=((2*pi*1i*fre).^-1).*resp1*gai1*digsen1;
+    else
+        resp1=resp1*gai1*digsen1;
+    end
+    
+    digsen2=getdigitmodel(digitizer2);
+    if(~strcmp(sensor2,'Epi-EST') && ~strcmp(sensor2,'Titan') && ...
+            ~strcmp(sensor2,'CMG-5T') && ~strcmp(sensor2,'147-01/3') && ...
+            ~strcmp(sensor2,'PA-23'))
+        resp2=((2*pi*1i*fre).^-1).*resp2*gai2*digsen2;
+    else
+        resp2=resp2*gai2*digsen2;
+    end
+    
+    digsen3=getdigitmodel(digitizer3);
+    if(~strcmp(sensor3,'Epi-EST') && ~strcmp(sensor3,'Titan') && ...
+            ~strcmp(sensor3,'CMG-5T') && ~strcmp(sensor3,'147-01/3') && ...
+            ~strcmp(sensor3,'PA-23'))
+        resp3=((2*pi*1i*fre).^-1).*resp3*gai3*digsen3;
+    else
+        resp3=resp3*gai3*digsen3;
+    end
+
+    
+    np1=10*log10((pspec1 - pspec21.*pspec13./pspec23)./(abs(resp1).^2));
+    np2=10*log10((pspec2 - conj(pspec23).*pspec21./conj(pspec13))./(abs(resp2).^2));
+    np3=10*log10((pspec3 - pspec23.*conj(pspec13)./pspec21)./(abs(resp3).^2));
+    
+    
+    pspec1=10*log10(pspec1./(abs(resp1).^2));
+    pspec2=10*log10(pspec2./(abs(resp2).^2));
+    pspec3=10*log10(pspec3./(abs(resp3).^2));
+    per = 1./fre;
+    stahand1 = [ net1 ' ' sta1 ' ' loc1 ' ' chan1 ' ' year1 ' ' day1];
+    stahand2 = [ net2 ' ' sta2 ' ' loc2 ' ' chan2 ' ' year2 ' ' day2];
+    stahand3 = [ net3 ' ' sta3 ' ' loc3 ' ' chan3 ' ' year3 ' ' day3];
+    tseries1 = detrend(noisedata1/digsen1);
+    tseries2 = detrend(noisedata2/digsen2);
+    tseries3 = detrend(noisedata3/digsen3);
+    if(lowpass)
+        h=fdesign.lowpass('N,F3db',8,0.1,sps);  
+        d=design(h,'FIR');
+        tseries1 = filtfilt(d.Numerator,1,tseries1);
+        tseries2 = filtfilt(d.Numerator,1,tseries2);
+        tseries3 = filtfilt(d.Numerator,1,tseries3);
+    end
+    ttime1 = noisetime1;
+    mabsvolt1 = mean(abs(tseries1));
+    ttime2 = noisetime2;
+    mabsvolt2 = mean(abs(tseries2));
+    ttime3 = noisetime3;
+    mabsvolt3 = mean(abs(tseries3));
+end
+
+
+
